5417bf8f5562af9a6ca4c97c4da8f88a32e92f7a
Ana Rubio Ruiz <Ana.Rubio@uclm.es>
2025-03-19 18:01:10 +0100

    add basic EchoUDPClient

---
+++ 5-UDPSockets/EchoUDPBasicClient.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+import java.net.DatagramSocket;
+import java.net.DatagramPacket;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+public class EchoUDPBasicClient {
+    public static void main(String[] args) throws IOException {
+        if (args.length < 2) {
+            System.err.println("Usage: java EchoUDPClient <text> <host> [<port>]");
+            System.exit(1);
+        }
+
+        String text = args[0];
+        String host = args[1];
+        int port = args.length > 2 ? Integer.parseInt(args[2]) : 10000;
+
+        DatagramSocket socket = new DatagramSocket();
+        DatagramPacket in_packet = new DatagramPacket(new byte[1024], 1024);
+        DatagramPacket out_packet = new DatagramPacket(
+            text.getBytes("UTF-8"),
+            text.length(),
+            java.net.InetAddress.getByName(host), port
+        );
+
+        socket.send(out_packet);
+        System.out.println("Sent: " + text);
+
+        socket.receive(in_packet);
+        System.out.println("Received: "
+            + new String(in_packet.getData(), 0, in_packet.getLength(), "UTF-8"));
+
+    }
+}
